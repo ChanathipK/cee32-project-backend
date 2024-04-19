696d03aaa052460ada003600629d2bc6517a669a
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2024-04-19 17:46:01 +0700

    add body-parser

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 // dotenv is used to load variable from `.env` or `.env.*` into `process` global variable
 require('dotenv').config()
 
+// setup cors
 app.use(cors());
+
+// setup body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Init Project");
