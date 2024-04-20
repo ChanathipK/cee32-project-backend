0663af4c41d5189ab2391033381edf4cb34c1882
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2024-04-20 11:55:56 +0700

    create schema and export model

--- src/parties.js
+++ src/parties.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const partySchema = mongoose.Schema({
+    users: {
+        type: [mongoose.Types.ObjectId],
+        unique: true,
+    }
+});
+
+const Party = mongoose.Model("party", partySchema);
+module.exports = Party;
--- src/users.js
+++ src/users.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    
-})
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    attack: {
+        type: Number,
+        required: true,
+    },
+    defence: {
+        type: Number,
+        required: true,
+    },
+    hp: {
+        type: Number,
+        required: true,
+    }
+});
+
+const User = mongoose.Model("user", userSchema);
+module.exports = User;
