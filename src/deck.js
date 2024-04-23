0e63e958d8b2a7ebb87bfcedb49bad6315914041
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2024-04-23 19:24:22 +0700

    update deck descriptions

--- src/deck.js
+++ src/deck.js
@@ -1,11 +1,11 @@
-// (+1 atk for 3 turns): 1,  20 cards
-// (+2 atk for 2 turns): 2,  10 cards
-// (+1 atk permanent):   3,  5  cards
-// (-1 atk for 2 turns): 4,  15 cards
-// (+1 def for 3 turns): 5,  12 cards
-// (+2 def for 2 turns): 6,  6  cards
-// (+1 def permanent):   7,  3  cards
-// (-1 def for 2 turns): 8,  9  cards
+// (+1 atk): 1,  20 cards
+// (+2 atk): 2,  10 cards
+// (+1 atk): 3,  5  cards
+// (-1 atk): 4,  15 cards
+// (+1 def): 5,  12 cards
+// (+2 def): 6,  6  cards
+// (+1 def): 7,  3  cards
+// (-1 def): 8,  9  cards
 // (skip target 1 turn): 9,  10 cards
 // (penertrate 1 time):  10, 10 cards
 let cards = [];
